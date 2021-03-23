91aebbacd5d593ef091c12481afb1bbe3748ed97
Douglas Henrique <dev.dhbarboza@gmail.com>
2021-03-22 22:06:15 -0300

    Create Widget Question

---
+++ quiz_app/lib/question.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Question extends StatelessWidget {
+  // Declarando variável do Tipo caracter:
+  final String txt;
+
+  // Chamo o Construtor:
+  Question(this.txt);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
