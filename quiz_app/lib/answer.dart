44db710336a6bded4ffad7a393bf638a7696d2e1
Douglas Henrique <dev.dhbarboza@gmail.com>
2021-03-23 22:18:56 -0300

    Container de resposta atualizado

--- quiz_app/lib/answer.dart
+++ quiz_app/lib/answer.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
+  // Declarando variável:
   final String txt;
 
-  Answer(this.txt);
+  // Definindo Função do onPressed:
+  final void Function() fnSelected;
+
+  // Chamando construtor:
+  Answer(this.txt, this.fnSelected);
 
   @override
   Widget build(BuildContext context) {
-    return RaisedButton(
-      child: Text(txt),
-      onPressed: _answer,
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: RaisedButton(
+        textColor: Colors.white,
+        color: Colors.blue[500],
+        child: Text(txt),
+        onPressed: fnSelected,
+      ),
     );
   }
 }
