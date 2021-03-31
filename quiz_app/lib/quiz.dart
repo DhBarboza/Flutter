438c77f0a4048c11f4ac82d55fd5ce15e2ec9677
Douglas Henrique <dev.dhbarboza@gmail.com>
2021-03-30 22:41:59 -0300

    Adicionado widget de Resultado

---
+++ quiz_app/lib/quiz.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import './question.dart';
+import './answer.dart';
+
+class Quiz extends StatelessWidget {
+  final List<Map<String, Object>> questions;
+  final int questionsSelected;
+  final void Function() answer;
+
+  Quiz({
+    @required this.questions,
+    @required this.questionsSelected,
+    @required this.answer,
+  });
+
+  // Crio uma função para definir se há pergunta selecionada:
+  bool get hasQuestionsSelected {
+    return questionsSelected < questions.length;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Criando uma Lista para receber as respostas?
+    List<String> listAnswers =
+        hasQuestionsSelected ? questions[questionsSelected]['answers'] : null;
+
+    return Column(
+      children: [
+        Question(questions[questionsSelected]['text']),
+
+        // Argumento para que todos os elementos da lista sejam passados:
+        ...listAnswers.map((text) => Answer(text, answer)).toList(),
+      ],
+    );
+  }
+}
