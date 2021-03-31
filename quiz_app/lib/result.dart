ef0ce463f9efdbdf8ddbe6d67bbb2ba47e8eb933
Douglas Henrique <dev.dhbarboza@gmail.com>
2021-03-30 21:47:00 -0300

    criado widget resultado

---
+++ quiz_app/lib/result.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Results extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Informações cadastradas',
+        style: TextStyle(
+          fontSize: 28,
+        ),
+      ),
+    );
+  }
+}
